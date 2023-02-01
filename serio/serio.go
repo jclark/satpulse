fdc6468a0853392bfed71d60c819452e8bd1b99d
James Clark <jjc@jclark.com>
2023-02-01 12:58:06 +0700

    Make term be anon member of Port

--- serio/serio.go
+++ serio/serio.go
@@ -17,34 +17,26 @@ const maxWriteLen = 4096
 const scanBufSize = 16
 
 type Port struct {
-	t    *term.Term
-	path string
+	term.Term
 }
 
 func Open(path string) (*Port, error) {
-	t, err := term.Open(path, term.RawMode, term.NoFlowControl, term.ReadTimeout(readTimeout))
+	p := &Port{}
+	err := p.Term.Init(path, term.RawMode, term.NoFlowControl, term.ReadTimeout(readTimeout))
 	if err != nil {
 		return nil, err
 	}
-	err = t.Flush()
+	err = p.Flush()
 	if err != nil {
-		t.Restore()
-		t.Close()
+		p.Restore()
+		p.Close()
 		return nil, err
 	}
-	return &Port{t: t, path: path}, nil
-}
-
-func (p *Port) Restore() error {
-	return p.t.Restore()
-}
-
-func (p *Port) Close() error {
-	return p.t.Close()
+	return p, nil
 }
 
 func (p *Port) StartRead(ctx context.Context) chan scan.Frame {
-	scanner := scan.New(p.t, scanBufSize)
+	scanner := scan.New(&p.Term, scanBufSize)
 	c := make(chan scan.Frame, 1) // XXX think about the buffering
 	go readWorker(ctx, scanner, c)
 	return c
@@ -92,7 +84,6 @@ func (p *Port) WriteAsync(ctx context.Context, frames [][]byte) <-chan error {
 func (p *Port) Write(ctx context.Context, buf []byte) (int, error) {
 	total := 0
 	lg := slog.FromContext(ctx)
-	t := p.t
 	for len(buf) > 0 {
 
 		// Semantics of Unix write and Go Write are not the same:
@@ -101,7 +92,7 @@ func (p *Port) Write(ctx context.Context, buf []byte) (int, error) {
 		if len(buf) > maxWriteLen {
 			wBuf = wBuf[0:maxWriteLen]
 		}
-		n, err := t.Write(wBuf)
+		n, err := p.Term.Write(wBuf)
 		if err == io.ErrShortWrite && n > 0 {
 			err = nil
 		}
@@ -118,55 +109,20 @@ func (p *Port) Write(ctx context.Context, buf []byte) (int, error) {
 
 func (p *Port) Drain(tx context.Context) error {
 	lg := slog.FromContext(tx)
-	t := p.t
 	for {
 		select {
 		case <-tx.Done():
 			return tx.Err()
 		default:
 		}
-		n, err := t.Buffered()
+		n, err := p.Term.Buffered()
 		if err != nil || n == 0 {
 			break
 		}
 		lg.Debug("serialBufferedBytes", "n", n)
 		time.Sleep(time.Microsecond * 10)
-		n, _ = t.Buffered()
+		n, _ = p.Term.Buffered()
 		lg.Debug("drainBufferedBytes", "n", n)
 	}
 	return nil
-}
-
-type DevKind int
-
-const (
-	DevUnknown DevKind = iota
-	DevUART
-	DevUSB
-	DevUSBtoUART
-	DevBT
-)
-
-func (p *Port) DevKind() DevKind {
-	s := unix.Stat_t{}
-	err := unix.Stat(p.path, &s)
-	if err != nil {
-		return DevUnknown
-	}
-	// See https://www.kernel.org/doc/html/latest/admin-guide/devices.html
-	switch unix.Major(s.Dev) {
-	case 4, 5:
-		if unix.Minor(s.Dev) >= 64 { // ttyS0, /dev/ttycua0
-			return DevUART
-		}
-	case 166, 167: // USB ACM "modem" /dev/ttyACM0
-		return DevUSB
-	case 188, 189: // USB serial converter /dev/ttyUSB0
-		return DevUSBtoUART
-	case 204, 205: // low-density serial port (Raspberry Pi uses /dev/ttyAMA0)
-		return DevUART
-	case 216, 217: // Bluetooth RFCOMM /dev/rfcomm0
-		return DevBT
-	}
-	return DevUnknown
 }
