e0383d24850914eac7e20caec0344aa23f2aaafd
James Clark <jjc@jclark.com>
2023-02-01 09:54:09 +0700

    Rework term

--- term/term.go
+++ term/term.go
@@ -2,6 +2,7 @@ package term
 
 import (
 	"os"
+	"time"
 
 	"github.com/pkg/term/termios"
 	"golang.org/x/sys/unix"
@@ -13,92 +14,155 @@ type Term struct {
 	attr unix.Termios
 }
 
-func OpenRaw(path string) (*Term, error) {
-	fd, err := unix.Open(path, unix.O_RDWR|unix.O_NOCTTY, 0)
+func OpenRaw(path string, timeout time.Duration) (*Term, error) {
+	t := new(Term)
+	err := t.InitRaw(path, timeout)
 	if err != nil {
-		return nil, &os.PathError{
-			Op:   "open",
-			Path: path,
-			Err:  err,
-		}
-	}
-	p := &Term{path: path, fd: fd}
-	err = termios.Tcgetattr(p.ufd(), &p.attr)
-	if err != nil {
-		unix.Close(fd)
-		return nil, p.wrapErr(err, "tcgetattr")
-	}
-	rawAttr := p.attr
-	termios.Cfmakeraw(&rawAttr)
-	// This means read with timeout of 11 tenths of a second
-	rawAttr.Cc[unix.VTIME] = 11
-	rawAttr.Cc[unix.VMIN] = 0
-	err = p.setAttr(&rawAttr)
-	if err != nil {
-		unix.Close(fd)
 		return nil, err
 	}
-	return p, nil
+	return t, nil
 }
 
-func (p *Term) ufd() uintptr {
-	return uintptr(p.fd)
+// A tenth of a second
+const decisecond = time.Second / 10
+
+func (t *Term) InitRaw(path string, timeout time.Duration) (err error) {
+	t.path = path
+	// XXX should open non-blocking and then change to blocking with fcntl
+	// in case CLOCAL is not set
+	fd, err := unix.Open(path, unix.O_RDWR|unix.O_NOCTTY, 0)
+	if err != nil {
+		err = t.wrapErr(err, "open")
+		return
+	}
+	t.fd = fd
+	defer func() {
+		if err != nil {
+			unix.Close(fd)
+		}
+	}()
+	err = termios.Tcgetattr(t.ufd(), &t.attr)
+	if err != nil {
+		err = t.wrapErr(err, "tcgetattr")
+		return
+	}
+	rawAttr := t.attr
+	termios.Cfmakeraw(&rawAttr)
+	// VTIME is a uint8 in units of 1/10th of a second
+	rawAttr.Cc[unix.VTIME] = uint8Clamp(int64(timeout.Round(decisecond) / decisecond))
+	rawAttr.Cc[unix.VMIN] = 0
+	// XXX turn of IXOFF
+	err = t.setAttr(&rawAttr)
+	return
 }
 
-func (p *Term) Read(buf []byte) (n int, err error) {
+func uint8Clamp(v int64) uint8 {
+	if v > 255 {
+		return 255
+	}
+	if v < 0 {
+		return 0
+	}
+	return uint8(v)
+}
+
+func (t *Term) ufd() uintptr {
+	return uintptr(t.fd)
+}
+
+func (t *Term) Read(buf []byte) (n int, err error) {
 	for {
-		n, err = unix.Read(p.fd, buf)
+		n, err = unix.Read(t.fd, buf)
 		if err != unix.EINTR {
 			break
 		}
 	}
 	if err != nil {
-		err = p.wrapErr(err, "read")
+		err = t.wrapErr(err, "read")
 	}
 	return
 }
 
-func (p *Term) Write(buf []byte) (int, error) {
+func (t *Term) Write(buf []byte) (int, error) {
 	total := 0
 	for len(buf) > 0 {
 		// Semantics of Unix write and Go Write are not the same:
 		// Unix can write less than requested amount without its being an error.
-		n, err := unix.Write(p.fd, buf)
+		n, err := unix.Write(t.fd, buf)
 		if err == nil {
 			total += n
 			buf = buf[n:]
 		} else if err != unix.EINTR {
-			return total, p.wrapErr(err, "write")
+			return total, t.wrapErr(err, "write")
 		}
 	}
 	return total, nil
 }
 
+func (t *Term) Buffered() (int, error) {
+	return termios.Tiocoutq(t.ufd())
+}
+
+func (t *Term) Restore() error {
+	return t.setAttr(&t.attr)
+}
+
 // Close resets the port's attributes and then closes it
-func (p *Term) Close() (err error) {
-	err = p.setAttr(&p.attr)
-	e2 := unix.Close(int(p.fd))
-	if e2 != nil && err == nil {
-		err = p.wrapErr(e2, "close")
-	}
-	return
+func (t *Term) Close() error {
+	fd := int(t.fd)
+	t.fd = -1
+	return t.wrapErr(unix.Close(fd), "close")
 }
 
-func (p *Term) Flush() error {
-	return p.wrapErr(termios.Tcflush(p.ufd(), unix.TCIOFLUSH), "tcflush")
+func (t *Term) Flush() error {
+	return t.wrapErr(termios.Tcflush(t.ufd(), unix.TCIOFLUSH), "tcflush")
 }
 
-func (p *Term) setAttr(attr *unix.Termios) error {
-	return p.wrapErr(termios.Tcsetattr(p.ufd(), termios.TCSANOW, attr), "tcsetattr")
+func (t *Term) setAttr(attr *unix.Termios) error {
+	return t.wrapErr(termios.Tcsetattr(t.ufd(), termios.TCSANOW, attr), "tcsetattr")
 }
 
-func (p *Term) wrapErr(err error, op string) error {
+func (t *Term) wrapErr(err error, op string) error {
 	if err == nil {
 		return err
 	}
 	return &os.PathError{
 		Op:   op,
-		Path: p.path,
+		Path: t.path,
 		Err:  err,
 	}
+}
+
+type DevKind int
+
+const (
+	DevUnknown DevKind = iota
+	DevUART
+	DevUSB
+	DevUSBtoUART
+	DevBT
+)
+
+func (t *Term) DevKind() DevKind {
+	s := unix.Stat_t{}
+	err := unix.Fstat(t.fd, &s)
+	if err != nil {
+		return DevUnknown
+	}
+	// See https://www.kernel.org/doc/html/latest/admin-guide/devices.html
+	switch unix.Major(s.Dev) {
+	case 4, 5:
+		if unix.Minor(s.Dev) >= 64 { // ttyS0, /dev/ttycua0
+			return DevUART
+		}
+	case 166, 167: // USB ACM "modem" /dev/ttyACM0
+		return DevUSB
+	case 188, 189: // USB serial converter /dev/ttyUSB0
+		return DevUSBtoUART
+	case 204, 205: // low-density serial port (Raspberry Pi uses /dev/ttyAMA0)
+		return DevUART
+	case 216, 217: // Bluetooth RFCOMM /dev/rfcomm0
+		return DevBT
+	}
+	return DevUnknown
 }
