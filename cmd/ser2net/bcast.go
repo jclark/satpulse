04dfc40fd8d9afea49e0c46ab447d67e4314ba64
James Clark <jjc@jclark.com>
2023-02-12 11:22:32 +0700

    Use reflect.SelectCase

--- cmd/ser2net/bcast.go
+++ cmd/ser2net/bcast.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"reflect"
 
 	"github.com/jclark/gps2phc/logctx"
 	"golang.org/x/exp/constraints"
@@ -37,59 +38,70 @@ func (b *bcast) run(ctx context.Context) {
 	}()
 	lg := logctx.FromContext(ctx)
 	for {
-		select {
-		case s := <-b.subscribe:
+		cases := []reflect.SelectCase{
+			{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(b.subscribe)},
+			{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(b.unsubscribe)},
+			{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(b.msg)},
+			{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ctx.Done())},
+		}
+		subscribersToDo := []*subscriber{}
+		qStartIndex := b.nextQIndex - len(b.q)
+		for _, s := range b.subscribers {
+			if s.nextSendIndex < b.nextQIndex {
+				subscribersToDo = append(subscribersToDo, s)
+				sc := reflect.SelectCase{
+					Dir:  reflect.SelectSend,
+					Chan: reflect.ValueOf(s.c),
+					Send: reflect.ValueOf(b.q[s.nextSendIndex-qStartIndex]),
+				}
+				cases = append(cases, sc)
+			}
+		}
+		chosen, recv, ok := reflect.Select(cases)
+		lg.Debug("bcastSelect", "chosen", chosen)
+		switch chosen {
+		case 0: // subscribe
+			s := recv.Interface().(chan<- []byte)
 			b.subscribers = append(b.subscribers, &subscriber{s, b.nextQIndex})
 			lg.Debug("subscribe", "chan", s)
-		case s := <-b.unsubscribe:
+		case 1: // unsubscribe
+			s := recv.Interface().(chan<- []byte)
 			i := b.subscriberIndex(s)
 			if i < 0 {
 				break
 			}
 			b.subscribers = append(b.subscribers[:i], b.subscribers[i+1:]...)
 			lg.Debug("unsubscribe", "chan", s)
-		case m, ok := <-b.msg:
+		case 2: // msg
 			if !ok {
 				return
 			}
+			m := recv.Interface().([]byte)
 			if len(b.subscribers) != 0 {
 				b.q = append(b.q, m)
 				b.nextQIndex++
 			}
-		case <-ctx.Done():
+		case 3: // Done
 			return
-		default:
+		default: // send to subscribe
+			subscribersToDo[chosen-4].nextSendIndex++
+			b.trimQ()
 		}
-		for {
-			progress := false
-			needIndex := b.nextQIndex
-			for _, s := range b.subscribers {
-				if s.nextSendIndex < b.nextQIndex {
-					lg.Debug("bcast", "nextSendIndex", s.nextSendIndex, "nextQIndex", b.nextQIndex, "len(b.q)", len(b.q))
-					qStartIndex := b.nextQIndex - len(b.q)
-					select {
-					case s.c <- b.q[s.nextSendIndex-qStartIndex]:
-						s.nextSendIndex++
-						progress = true
-					case <-ctx.Done():
-						return
-					default:
-					}
-					needIndex = min(needIndex, s.nextSendIndex)
-				}
-			}
-			nNeed := b.nextQIndex - needIndex
-			if nNeed == 0 {
-				b.q = b.q[:0]
-			} else if nDiscard := len(b.q) - nNeed; nDiscard > 0 {
-				oldQ := b.q[nDiscard:]
-				b.q = b.q[0:nNeed]
-				copy(b.q, oldQ)
-			}
-			if !progress {
-				break
-			}
-		}
+	}
+}
+
+func (b *bcast) trimQ() {
+	needIndex := b.nextQIndex
+	for _, s := range b.subscribers {
+		needIndex = min(needIndex, s.nextSendIndex)
+	}
+	nNeed := b.nextQIndex - needIndex
+	if nNeed == 0 {
+		b.q = b.q[:0]
+	} else if nDiscard := len(b.q) - nNeed; nDiscard > 0 {
+		oldQ := b.q[nDiscard:]
+		b.q = b.q[0:nNeed]
+		copy(b.q, oldQ)
 	}
 }
 
