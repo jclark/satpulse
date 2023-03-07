85c85157144847a19c499cba9192ecb2baf74ddd
James Clark <jjc@jclark.com>
2023-03-07 09:52:08 +0700

    Remove unused code

--- internal/ubx/bin/bin.go
+++ internal/ubx/bin/bin.go
@@ -5,8 +5,6 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
-	"regexp"
-	"strconv"
 	"strings"
 )
 
@@ -758,53 +756,4 @@ func checksum[B Bytes](bytes B) (ckA, ckB byte) {
 		ckB += ckA
 	}
 	return
-}
-
-// Latin1ZString create a string from a ISO Latin-1, nul-terminated byte slice.
-// This can be used for the fields of MonVer
-func Latin1ZToString(chars []byte) string {
-	r := make([]rune, 0)
-	for _, ch := range chars {
-		if ch == 0 {
-			break
-		}
-		r = append(r, rune(ch))
-	}
-	return string(r)
-}
-
-type ProtVer struct {
-	major, minor byte
-}
-
-func (v ProtVer) String() string {
-	return fmt.Sprintf("%d.%0d", v.major, v.minor)
-}
-
-var protverRegexp = regexp.MustCompile(`^PROTVER[= ]([1-9][0-9]?)\.([0-9][0-9])$`)
-
-func (m *MonVer) ProtVer() ProtVer {
-	submatches := m.findExtension(protverRegexp)
-	if submatches == nil {
-		return ProtVer{}
-	}
-	return ProtVer{mustAtob(submatches[1]), mustAtob(submatches[2])}
-}
-
-func mustAtob(s string) byte {
-	n, err := strconv.Atoi(s)
-	if err != nil {
-		panic(`could not convert UBX "` + s + `" to integer: ` + err.Error())
-	}
-	return byte(n)
-}
-
-func (m *MonVer) findExtension(re *regexp.Regexp) []string {
-	for _, buf := range m.Extension {
-		submatches := re.FindStringSubmatch(Latin1ZToString(buf[:]))
-		if submatches != nil {
-			return submatches
-		}
-	}
-	return nil
 }
