ce631f66b992ce260b21410040f5ce25e96d633c
James Clark <jjc@jclark.com>
2023-03-09 17:09:28 +0700

    Start on ublox configuration

---
+++ internal/ubx/cfg/cfg.go
@@ -0,0 +1,248 @@
+package cfg
+
+import (
+	"encoding/binary"
+	"fmt"
+	"math"
+)
+
+type Desc interface {
+	key() uint32
+	MarshalValue(v any) ([]byte, error)
+}
+
+type U uint32
+type I uint32
+type L uint32
+type R uint32
+
+type EDesc struct {
+	k      uint32
+	values []string
+}
+
+func E(k uint32, values ...string) *EDesc {
+	return &EDesc{k, values}
+}
+
+func (d U) key() uint32 {
+	return uint32(d)
+}
+
+func (d I) key() uint32 {
+	return uint32(d)
+}
+
+func (d R) key() uint32 {
+	return uint32(d)
+}
+
+func (d L) key() uint32 {
+	return uint32(d)
+}
+
+type Schema struct {
+	groups map[string]map[string]Desc
+}
+
+func (s *Schema) Marshal(cfg map[string]map[string]any) ([]byte, error) {
+	var bytes []byte
+	for g, v := range cfg {
+		desc, ok := s.groups[g]
+		if !ok {
+			return nil, fmt.Errorf("unknown group %s", g)
+		}
+		b, err := marshalGroup(desc, v)
+		if err != nil {
+			return nil, err
+		}
+		bytes = append(bytes, b...)
+	}
+	return bytes, nil
+}
+
+func marshalGroup(desc map[string]Desc, cfg map[string]any) ([]byte, error) {
+	var bytes []byte
+	for s, v := range cfg {
+		d, ok := desc[s]
+		if !ok {
+			return nil, fmt.Errorf("unknown key %s", s)
+		}
+		b, err := marshalKey(d)
+		if err != nil {
+			return nil, err
+		}
+		bytes = append(bytes, b...)
+		b, err = d.MarshalValue(v)
+		if err != nil {
+			return nil, err
+		}
+		bytes = append(bytes, b...)
+	}
+	return bytes, nil
+}
+
+func marshalKey(d Desc) ([]byte, error) {
+	bytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bytes, d.key())
+	return bytes, nil
+}
+
+func (d *EDesc) key() uint32 {
+	return d.k
+}
+
+func (d *EDesc) MarshalValue(v any) ([]byte, error) {
+	s, ok := v.(string)
+	if !ok {
+		return nil, fmt.Errorf("cannot marshal %T to enum", v)
+	}
+	for i, value := range d.values {
+		if s == value {
+			return marshalUnsigned(d.k, uint64(i))
+		}
+	}
+	return nil, fmt.Errorf("invalid value %s for enum", s)
+}
+
+func (d L) MarshalValue(v any) ([]byte, error) {
+	b, ok := v.(bool)
+	if !ok {
+		return nil, fmt.Errorf("cannot marshal %T to bool", v)
+	}
+	return []byte{boolToByte(b)}, nil
+}
+
+func boolToByte(b bool) byte {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func (d R) MarshalValue(v any) ([]byte, error) {
+	switch valueBits(d.key()) {
+	case 64:
+		f, ok := v.(float64)
+		if !ok {
+			return nil, fmt.Errorf("cannot marshal %T to float64", v)
+		}
+		bytes := make([]byte, 8)
+		u8 := math.Float64bits(f)
+		binary.LittleEndian.PutUint64(bytes, u8)
+		return bytes, nil
+	case 32:
+		f, ok := v.(float32)
+		if !ok {
+			return nil, fmt.Errorf("cannot marshal %T to float32", v)
+		}
+		bytes := make([]byte, 4)
+		u4 := math.Float32bits(f)
+		binary.LittleEndian.PutUint32(bytes, u4)
+		return bytes, nil
+	default:
+		panic(fmt.Sprintf("invalid key ID 0x%x for float type", d.key()))
+	}
+}
+
+func (d U) MarshalValue(v any) ([]byte, error) {
+	n, ok := unsignedWiden(v)
+	if !ok {
+		s, ok := signedWiden(v)
+		if !ok {
+			return nil, fmt.Errorf("cannot marshal %T to unsigned integer", v)
+		}
+		if s < 0 {
+			return nil, fmt.Errorf("cannot marshal negative integer %d to unsigned integer", s)
+		}
+		n = uint64(s)
+	}
+	bits := valueBits(d.key())
+	if bits < 64 && n >= 1<<bits {
+		return nil, fmt.Errorf("value %d too large for U%d", n, bits)
+	}
+	return marshalUnsigned(d.key(), n)
+}
+
+func (d I) MarshalValue(v any) ([]byte, error) {
+	n, ok := signedWiden(v)
+	if !ok {
+		s, ok := unsignedWiden(v)
+		if !ok {
+			return nil, fmt.Errorf("cannot marshal %T to signed integer", v)
+		}
+		if s > math.MaxInt64 {
+			return nil, fmt.Errorf("cannot marshal unsigned integer %d to signed integer", s)
+		}
+		n = int64(s)
+	}
+	bits := valueBits(d.key())
+	if bits < 64 && (n >= 1<<(bits-1) || n < -1<<(bits-1)) {
+		return nil, fmt.Errorf("value %d does not fit in I%d", n, bits)
+	}
+	return marshalUnsigned(d.key(), uint64(n))
+}
+
+func marshalUnsigned(k uint32, n uint64) ([]byte, error) {
+	switch valueBits(k) {
+	case 8:
+		return []byte{uint8(n)}, nil
+	case 16:
+		bytes := make([]byte, 2)
+		u2 := uint16(n)
+		binary.LittleEndian.PutUint16(bytes, u2)
+		return bytes, nil
+	case 32:
+		u4 := uint32(n)
+		bytes := make([]byte, 4)
+		binary.LittleEndian.PutUint32(bytes, u4)
+		return bytes, nil
+	default:
+		bytes := make([]byte, 8)
+		binary.LittleEndian.PutUint64(bytes, n)
+		return bytes, nil
+	}
+}
+
+func unsignedWiden(v any) (uint64, bool) {
+	switch n := v.(type) {
+	case uint:
+		return uint64(n), true
+	case uint8:
+		return uint64(n), true
+	case uint16:
+		return uint64(n), true
+	case uint32:
+		return uint64(n), true
+	case uint64:
+		return n, true
+	}
+	return 0, false
+}
+
+func signedWiden(v any) (int64, bool) {
+	switch n := v.(type) {
+	case int:
+		return int64(n), true
+	case int8:
+		return int64(n), true
+	case int16:
+		return int64(n), true
+	case int32:
+		return int64(n), true
+	case int64:
+		return n, true
+	}
+	return 0, false
+}
+
+func valueBits(k uint32) int {
+	sz := int(k>>28) & 0x0f
+	switch sz {
+	case 1:
+		return 1
+	case 2, 3, 4, 5:
+		return 2 << sz
+	}
+	return 0
+}
---
+++ internal/ubx/cfg/cfg_test.go
@@ -0,0 +1,92 @@
+package cfg
+
+import (
+	"math"
+	"testing"
+
+	"golang.org/x/exp/slices"
+)
+
+type marshalTestCase struct {
+	schema Schema
+	config map[string]map[string]any
+	bytes  []byte
+}
+
+var schema = Schema{map[string]map[string]Desc{
+	"TMODE": {
+		"MODE":   E(0x20030001, "DISABLED", "SURVEY_IN", "FIXED"),
+		"ECEF_X": I(0x40030003),
+		"ECEF_Y": I(0x40030004),
+		"ECEF_Z": I(0x40030005),
+	},
+}}
+
+var marshalTestCases = []marshalTestCase{
+	{schema, map[string]map[string]any{"TMODE": {"MODE": "FIXED"}}, []byte{0x01, 0x00, 0x03, 0x20, 0x02}},
+}
+
+func TestMarshal(t *testing.T) {
+	for i, tc := range marshalTestCases {
+		bytes, err := tc.schema.Marshal(tc.config)
+		if err != nil {
+			if tc.bytes != nil {
+				t.Errorf("marshal test case %d marshal failed %v", i, err)
+			}
+		} else if tc.bytes == nil {
+			t.Errorf("marshal test case %d marshal succeeded unexpectedly", i)
+		} else if !slices.Equal(bytes, tc.bytes) {
+			t.Errorf("wrong result for marshal test case %d (got %v, expected %v)", i, bytes, tc.bytes)
+		}
+	}
+}
+
+func TestValueBits(t *testing.T) {
+	if valueBits(0x10260013) != 1 {
+		t.Error("expected valueBits(0x10260013) == 1")
+	}
+	if valueBits(0x30260015) != 16 {
+		t.Error("expected valueBits(0x30260015) == 16")
+	}
+	if valueBits(0x50110063) != 64 {
+		t.Error("expected valueBits(0x50110063) == 64")
+	}
+}
+
+func TestSignedWiden(t *testing.T) {
+	n, ok := signedWiden(-1)
+	if !ok || n != -1 {
+		t.Errorf("signedWiden(-1) = %d", n)
+	}
+	n, ok = signedWiden(int8(-1))
+	if !ok || n != -1 {
+		t.Errorf("signedWiden(int8(-1)) = %d", n)
+	}
+	n, ok = signedWiden(int32(1827186412))
+	if !ok || n != 1827186412 {
+		t.Errorf("signedWiden(int32(1827186412)) = %d", n)
+	}
+	n, ok = signedWiden(uint64(math.MaxUint64))
+	if ok {
+		t.Errorf("signedWiden(uint64(math.MaxUint64)) = %d", n)
+	}
+}
+
+func TestUnsignedWiden(t *testing.T) {
+	n, ok := unsignedWiden(uint(1))
+	if !ok || n != 1 {
+		t.Errorf("unsignedWiden(1) = %d", n)
+	}
+	n, ok = unsignedWiden(uint8(0))
+	if !ok || n != 0 {
+		t.Errorf("unsignedWiden(uint8(0)) = %v, %d", ok, n)
+	}
+	n, ok = unsignedWiden(uint32(1827186412))
+	if !ok || n != 1827186412 {
+		t.Errorf("unsignedWiden(uint32(1827186412)) = %d", n)
+	}
+	n, ok = unsignedWiden(int(0))
+	if ok {
+		t.Errorf("unsignedWiden(int(0)) = %d", n)
+	}
+}
