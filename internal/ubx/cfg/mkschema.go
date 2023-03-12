ac3c96e9f28a357e91464ebe6b72a2f45af1f153
James Clark <jjc@jclark.com>
2023-03-12 09:44:05 +0700

    Create separate schema directory

--- internal/ubx/cfg/mkschema.go
+++ internal/ubx/cfg/mkschema.go
@@ -5,40 +5,20 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"sort"
 	"strings"
 
-	"gopkg.in/yaml.v3"
+	"github.com/jclark/gps2phc/internal/ubx/cfg/schema"
 )
 
-type Item struct {
-	Key         uint32
-	Type        string
-	Description string
-	Unit        string
-	Scale       float64
-	Note        string
-	Min         float64
-	Max         float64
-	Condition   string
-	Constants   map[string]Constant
-}
-
-type Constant struct {
-	Value       uint64
-	Description string
-}
-
 func main() {
-	bytes, err := ioutil.ReadFile("cfg.yaml")
-	var schema map[string]map[string]Item
+	bytes, err := os.ReadFile("schema/cfg.yaml")
 	if err == nil {
-		err = yaml.Unmarshal(bytes, &schema)
-	}
-	if err == nil {
-		err = os.WriteFile("schema.go", []byte(fmtSchema(schema)), 0644)
+		schema, err := schema.UnmarshalYAML(bytes)
+		if err == nil {
+			err = os.WriteFile("schema.go", []byte(fmtSchema(schema)), 0644)
+		}
 	}
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
@@ -46,7 +26,7 @@ func main() {
 	}
 }
 
-func fmtSchema(schema map[string]map[string]Item) string {
+func fmtSchema(schema map[string]map[string]schema.Item) string {
 	groups := make([]string, 0, len(schema))
 	for name, m := range schema {
 		groups = append(groups, fmtGroup(name, m))
@@ -55,7 +35,7 @@ func fmtSchema(schema map[string]map[string]Item) string {
 	return fmt.Sprintf("package cfg\n\nvar schema = MustNewSchema(map[string]map[string]Desc{\n%s})\n", strings.Join(groups, ""))
 }
 
-func fmtGroup(name string, m map[string]Item) string {
+func fmtGroup(name string, m map[string]schema.Item) string {
 	lines := make([]string, 0, len(m))
 	for name, item := range m {
 		lines = append(lines, fmtItem(name, item))
@@ -64,11 +44,11 @@ func fmtGroup(name string, m map[string]Item) string {
 	return fmt.Sprintf("\t%q:{\n%s\t},\n", name, strings.Join(lines, ""))
 }
 
-func fmtItem(name string, item Item) string {
+func fmtItem(name string, item schema.Item) string {
 	return fmt.Sprintf("\t\t%q: %s(0x%x%s),\n", name, item.Type[:1], item.Key, fmtConsts(item.Constants))
 }
 
-func fmtConsts(cMap map[string]Constant) string {
+func fmtConsts(cMap map[string]schema.Constant) string {
 	if cMap == nil {
 		return ""
 	}
---
+++ internal/ubx/cfg/schema/schema.go
@@ -0,0 +1,33 @@
+package schema
+
+import (
+	"gopkg.in/yaml.v3"
+)
+
+type Item struct {
+	Key         uint32
+	Type        string
+	Description string
+	Unit        string
+	Scale       float64
+	Note        string
+	Min         float64
+	Max         float64
+	Condition   string
+	Constants   map[string]Constant
+}
+
+type Constant struct {
+	Value       uint64
+	Description string
+}
+
+func UnmarshalYAML(bytes []byte) (map[string]map[string]Item, error) {
+	var schema map[string]map[string]Item
+
+	err := yaml.Unmarshal(bytes, &schema)
+	if err != nil {
+		return nil, err
+	}
+	return schema, nil
+}
