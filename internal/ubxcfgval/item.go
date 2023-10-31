930ef254067321399ce96764b630d01851ca223e
James Clark <jjc@jclark.com>
2023-10-31 23:27:38 +0700

    Introduce ubxcfgval.Item type

---
+++ internal/ubxcfgval/item.go
@@ -0,0 +1,109 @@
+package ubxcfgval
+
+import (
+	"encoding"
+	"encoding/binary"
+	"fmt"
+	"sort"
+)
+
+type Key uint32
+type Item struct {
+	Key   Key
+	Value uint64
+}
+
+type Map map[Key]uint64
+
+var _ encoding.BinaryUnmarshaler = (*Map)(nil)
+var _ encoding.BinaryMarshaler = (*Map)(nil)
+
+func NewMap(items []Item) Map {
+	m := make(Map)
+	for _, it := range items {
+		m[it.Key] = it.Value
+	}
+	return m
+}
+
+func (mp *Map) Items() []Item {
+	m := *mp
+	items := make([]Item, 0, len(m))
+	for k, v := range m {
+		items = append(items, Item{k, v})
+	}
+	return items
+}
+
+func SortItems(items []Item) {
+	sort.Slice(items, func(i, j int) bool { return items[i].Key < items[j].Key })
+}
+
+func (mp *Map) MarshalBinary() ([]byte, error) {
+	items := mp.Items()
+	SortItems(items)
+	return MarshalItems(items)
+}
+
+func (mp *Map) UnmarshalBinary(data []byte) error {
+	items, err := UnmarshalItems(data)
+	if err != nil {
+		return err
+	}
+	*mp = NewMap(items)
+	return nil
+}
+
+func MarshalItems(items []Item) ([]byte, error) {
+	bytes := make([]byte, len(items)*12)
+	i := 0
+	for _, it := range items {
+		binary.LittleEndian.PutUint32(bytes[i:], uint32(it.Key))
+		i += 4
+		binary.LittleEndian.PutUint64(bytes[i:], it.Value)
+		i += it.Key.nValueBytes()
+	}
+	return bytes[0:i], nil
+}
+
+func UnmarshalItems(data []byte) ([]Item, error) {
+	items := make([]Item, 0, len(data)/12)
+	for len(data) > 4 {
+		k := Key(binary.LittleEndian.Uint32(data))
+		data = data[4:]
+		nBytes := k.nValueBytes()
+		if len(data) < nBytes {
+			return nil, fmt.Errorf("invalid data length for key 0x%x", k)
+		}
+		// this is not the most efficient way to do things, but this won't be a hotspot
+		valBytes := make([]byte, 8)
+		copy(valBytes, data[0:nBytes])
+		items = append(items, Item{k, binary.LittleEndian.Uint64(valBytes)})
+		data = data[nBytes:]
+	}
+	if len(data) > 0 {
+		return nil, fmt.Errorf("leftover data")
+	}
+	return items, nil
+}
+
+func (k Key) nValueBytes() int {
+	return (k.valueBits() + 7) / 8
+}
+
+func (k Key) valueBits() int {
+	sz := int(k>>28) & 0x0f
+	switch sz {
+	case 1:
+		return 1
+	case 2, 3, 4, 5:
+		return 2 << sz
+	}
+	return 0
+}
+
+func (k Key) MarshalBinary() ([]byte, error) {
+	bytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bytes, uint32(k))
+	return bytes, nil
+}
