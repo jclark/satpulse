6dc1869da842c2017a18024ea821a686e2a6430d
James Clark <jjc@jclark.com>
2024-01-01 11:44:22 +0700

    Fix last commit

--- cmd/devtest/devtest.go
+++ cmd/devtest/devtest.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"net"
 	"os"
 	"time"
 
-	"github.com/jclark/satpulse/netnotify"
+	"github.com/jclark/satpulse/internal/devnotify"
 )
 
-var ifName string
-var runTime = 30 * time.Second
+var runTime = 600 * time.Second
 
 func main() {
-	flag.StringVar(&ifName, "e", "eth0", "ethernet interface to monitor")
-	flag.Parse()
 	err := run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, os.Args[0]+":", err)
@@ -24,12 +19,7 @@ func main() {
 }
 
 func run() error {
-	iface, err := net.InterfaceByName(ifName)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("initial flags: %s\n", iface.Flags.String())
-	n, err := netnotify.OpenNotifier()
+	n, err := devnotify.Open()
 	if err != nil {
 		return err
 	}
@@ -42,13 +32,15 @@ func run() error {
 			}
 			if ev.Err != nil {
 				fmt.Printf("error: %v\n", ev.Err)
-			}
-			if ev.Index == iface.Index {
-				fmt.Printf("flags: %s\n", ev.Flags.String())
+			} else if ev.Path != "" {
+				fmt.Printf("device path: %s\n", ev.Path)
+			} else if ev.IfName != "" {
+				fmt.Printf("interface %d: %s\n", ev.IfIndex, ev.IfName)
 			}
 		case <-timeout:
 			fmt.Println("timeout: closing")
 			n.Close()
 		}
 	}
+
 }
