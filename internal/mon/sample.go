cd2b9962d63935b14e336d538cce5ef41b0dad1a
James Clark <jjc@jclark.com>
2024-02-22 19:56:06 +0700

    Refine isInSync
    Have separate limits on number of consecutive bad samples and number of bad samples.
    Fixes #34.

--- internal/mon/sample.go
+++ internal/mon/sample.go
@@ -40,9 +40,14 @@ func (w *sampleWindow) append(kind sampleKind, off float64, era ptime.Era) {
 }
 
 type sampleConfig struct {
-	maxOffset     float64
-	minGood       int
-	maxConsecBad  int
+	maxOffset float64
+	// minimum number of OK samples in window, within maxOffset, to be in sync
+	minGood int
+	// if we get more than maxConsecBad consecutive bad samples (before minGood), we are out of sync
+	// bad sample here means missing or outlier (something not fed to the servo) rather than a large offset
+	maxConsecBad int
+	// if we get more than maxTotalBad bad samples (before minGood), we are out of sync
+	maxTotalBad   int
 	madMultiple   float64
 	madMinSamples int
 }
@@ -51,9 +56,11 @@ var defaultSampleConfig = sampleConfig{
 	// For sync, require that the maximum absolute offset is less than 50ns.
 	// Reasoning is we are claiming 100ns accuracy, and we need to budget for other sources of error,
 	// specifically errors in GPS signal
+	// any offset > maxOffset, that is not an outlier, makes us out of sync
 	maxOffset:    50e-9,
 	minGood:      4,
-	maxConsecBad: holdoverSecs,
+	maxConsecBad: 7,
+	maxTotalBad:  12,
 	// Stable32 uses 5 here, but outliers for GPS are usually quite extreme compared to the normal offsets which are usually <30ns
 	// If it's too low, then during settling phase things can be incorrectly marked as outliers
 	madMultiple:   25,
@@ -63,7 +70,8 @@ var defaultSampleConfig = sampleConfig{
 func (w *sampleWindow) isInSync(curInSync bool, cfg *sampleConfig) bool {
 	inSync := false
 	nGood := 0
-	nBad := 0
+	nConsecBad := 0
+	nTotalBad := 0
 	w.iterate(func(i int, sample sampleData) bool {
 		switch sample.kind {
 		case sampleOK:
@@ -71,16 +79,17 @@ func (w *sampleWindow) isInSync(curInSync bool, cfg *sampleConfig) bool {
 				return false
 			}
 			nGood++
-			nBad = 0
+			nConsecBad = 0
 			if nGood >= cfg.minGood {
 				inSync = true
 				return false
 			}
 		default:
-			nBad++
+			nConsecBad++
+			nTotalBad++
 			// if we've gone out of sync, don't allow any bad (missing/outlier) values;
 			// otherwise allow up to holdoverSecs consecutive bad values
-			if !curInSync || nBad > cfg.maxConsecBad {
+			if !curInSync || nConsecBad > cfg.maxConsecBad || nTotalBad > cfg.maxTotalBad {
 				return false
 			}
 		}
