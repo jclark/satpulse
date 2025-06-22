4e759f01f1a5f81d458303d5d77f55b4a93356fe
James Clark <jjc@jclark.com>
2025-06-22 04:50:21 +0200

    Add m8-signal replay test

--- internal/gpscmd/replay_files_test.go
+++ internal/gpscmd/replay_files_test.go
@@ -1,13 +1,14 @@
-	package gpscmd
+package gpscmd
 
-	var replayFiles = []string{
-		"f9p-noop",
-		"f9p-signal", 
-		"f9p-reload",
-		"f9p-binary",
-		"f9p-raw-out",
-		"f9p-rtcm-out",
-		"f9p-pvt-out",
-		"f9t-signal",
-		"m8-speed",
-	}
+var replayFiles = []string{
+	"f9p-noop",
+	"f9p-signal",
+	"f9p-reload",
+	"f9p-binary",
+	"f9p-raw-out",
+	"f9p-rtcm-out",
+	"f9p-pvt-out",
+	"f9t-signal",
+	"m8-speed",
+	"m8-signal",
+}
