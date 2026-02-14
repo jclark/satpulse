3b55cef2e35e7410922f2a28f26a880d0070a670
James Clark <jjc@jclark.com>
2026-02-14 13:27:36 +0700

    Move time accuracy into NavEpochMsg.Acc alongside position/velocity accuracy

--- gps/internal/ubx/ubxtime_test.go
+++ gps/internal/ubx/ubxtime_test.go
@@ -3,6 +3,9 @@ package ubx
 import (
 	"testing"
 	"time"
+
+	"github.com/jclark/satpulse/gps/gpsprot"
+	"github.com/jclark/satpulse/gps/lib/opt"
 )
 
 func TestMSScaledTOW(t *testing.T) {
@@ -11,5 +14,14 @@ func TestMSScaledTOW(t *testing.T) {
 	result := msScaledTOW(ms)
 	if result != expected {
 		t.Errorf("scaledMSToNS(0x%X) = %d; expected %d", ms, result, expected)
+	}
+}
+
+func TestNavEpochTimeAcc(t *testing.T) {
+	var ne gpsprot.NavEpochMsg
+	navEpochTimeAcc(&ne, 23)
+	want := opt.Make(23 * time.Nanosecond)
+	if ne.Acc.Time != want {
+		t.Errorf("Acc.Time = %v, want %v", ne.Acc.Time, want)
 	}
 }
