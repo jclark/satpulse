1c4202c168936e06db5bf98cdda695c27d28ec75
James Clark <jjc@jclark.com>
2026-02-20 09:13:27 +0700

    Add BestGNSSPos/Vel IDs and sort novmsg others alphabetically

--- gps/lib/novmsg/other.go
+++ gps/lib/novmsg/other.go
@@ -2,35 +2,39 @@ package novmsg
 
 // Message IDs for known but unimplemented NovAtel-compatible binary messages.
 const (
-	VersionID      MsgID = 37   // OEM7, SinoGNSS
-	RawEphemID     MsgID = 41   // OEM7, SinoGNSS, RTKLIB
-	RangeID        MsgID = 43   // OEM7, SinoGNSS, RTKLIB
-	PsrPosID       MsgID = 47   // OEM7, SinoGNSS
+	BdsEphemerisID MsgID = 1696 // OEM7, Bynav, RTKLIB
+	BestGNSSPosID  MsgID = 1429 // OEM7, Bynav
+	BestGNSSVelID  MsgID = 1430 // OEM7, Bynav
 	BestVelID      MsgID = 99   // OEM7, SinoGNSS
-	PsrVelID       MsgID = 100  // OEM7, Bynav, SinoGNSS
-	RangeCmpID     MsgID = 140  // OEM7, Bynav, SinoGNSS, Unicore, RTKLIB
-	PsrDopID       MsgID = 174  // OEM7, SinoGNSS
-	RefStationID   MsgID = 175  // OEM7, SinoGNSS
-	MarkTimeID     MsgID = 231  // OEM6, Bynav, SinoGNSS
+	GalEphemerisID MsgID = 1122 // OEM6, Bynav, SinoGNSS, RTKLIB
 	GloEphemerisID MsgID = 723  // OEM7, Bynav, SinoGNSS, RTKLIB
 	HeadingID      MsgID = 971  // OEM6, Bynav, SinoGNSS
-	GalEphemerisID MsgID = 1122 // OEM6, Bynav, SinoGNSS, RTKLIB
-	BdsEphemerisID MsgID = 1696 // OEM7, Bynav, RTKLIB
+	MarkTimeID     MsgID = 231  // OEM6, Bynav, SinoGNSS
+	PsrDopID       MsgID = 174  // OEM7, SinoGNSS
+	PsrPosID       MsgID = 47   // OEM7, SinoGNSS
+	PsrVelID       MsgID = 100  // OEM7, Bynav, SinoGNSS
+	RangeCmpID     MsgID = 140  // OEM7, Bynav, SinoGNSS, Unicore, RTKLIB
+	RangeID        MsgID = 43   // OEM7, SinoGNSS, RTKLIB
+	RawEphemID     MsgID = 41   // OEM7, SinoGNSS, RTKLIB
+	RefStationID   MsgID = 175  // OEM7, SinoGNSS
+	VersionID      MsgID = 37   // OEM7, SinoGNSS
 )
 
 func init() {
-	idNameMap[VersionID] = "VERSION"
-	idNameMap[RawEphemID] = "RAWEPHEM"
-	idNameMap[RangeID] = "RANGE"
-	idNameMap[PsrPosID] = "PSRPOS"
+	idNameMap[BdsEphemerisID] = "BDSEPHEMERIS"
+	idNameMap[BestGNSSPosID] = "BESTGNSSPOS"
+	idNameMap[BestGNSSVelID] = "BESTGNSSVEL"
 	idNameMap[BestVelID] = "BESTVEL"
-	idNameMap[PsrVelID] = "PSRVEL"
-	idNameMap[RangeCmpID] = "RANGECMP"
-	idNameMap[PsrDopID] = "PSRDOP"
-	idNameMap[RefStationID] = "REFSTATION"
-	idNameMap[MarkTimeID] = "MARKTIME"
+	idNameMap[GalEphemerisID] = "GALEPHEMERIS"
 	idNameMap[GloEphemerisID] = "GLOEPHEMERIS"
 	idNameMap[HeadingID] = "HEADING"
-	idNameMap[GalEphemerisID] = "GALEPHEMERIS"
-	idNameMap[BdsEphemerisID] = "BDSEPHEMERIS"
+	idNameMap[MarkTimeID] = "MARKTIME"
+	idNameMap[PsrDopID] = "PSRDOP"
+	idNameMap[PsrPosID] = "PSRPOS"
+	idNameMap[PsrVelID] = "PSRVEL"
+	idNameMap[RangeCmpID] = "RANGECMP"
+	idNameMap[RangeID] = "RANGE"
+	idNameMap[RawEphemID] = "RAWEPHEM"
+	idNameMap[RefStationID] = "REFSTATION"
+	idNameMap[VersionID] = "VERSION"
 }
