c3e40fdde2b73a1f830cb1a3d457b4dbc5c8014c
James Clark <jjc@jclark.com>
2026-02-23 22:44:56 +0700

    Support 460800 and 921600 baud on macOS and FreeBSD
    Split baudRates table out of term_bsd.go into platform-specific files.
    On Darwin, the B-constants are the numeric speed, so 460800/921600 can
    be added as literal values. On FreeBSD, unix.B460800/B921600 exist.

--- gps/lib/term/term_bsd.go
+++ gps/lib/term/term_bsd.go
@@ -15,30 +15,6 @@ func (ic *SerialICounter) errorCounts() (ec ErrorCounts) {
 	return
 }
 
-var baudRates = []struct {
-	b     uint32
-	speed int
-}{
-	{unix.B50, 50},
-	{unix.B75, 75},
-	{unix.B110, 110},
-	{unix.B134, 134},
-	{unix.B150, 150},
-	{unix.B200, 200},
-	{unix.B300, 300},
-	{unix.B600, 600},
-	{unix.B1200, 1200},
-	{unix.B1800, 1800},
-	{unix.B2400, 2400},
-	{unix.B4800, 4800},
-	{unix.B9600, 9600},
-	{unix.B19200, 19200},
-	{unix.B38400, 38400},
-	{unix.B57600, 57600},
-	{unix.B115200, 115200},
-	{unix.B230400, 230400},
-}
-
 func Speed(speed int) AttrSetter {
 	b, ok := speedToB(speed)
 	if !ok {
