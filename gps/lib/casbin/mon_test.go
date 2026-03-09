d01edb7182d934b495a20cabd4e6c443178246e5
James Clark <jjc@jclark.com>
2026-03-09 08:17:46 +0700

    Add MON-VER decoding to casbin with Latin1Z32 string type

---
+++ gps/lib/casbin/mon_test.go
@@ -0,0 +1,33 @@
+package casbin
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+)
+
+func TestMonVerRoundtrip(t *testing.T) {
+	var sw, hw Latin1Z32
+	copy(sw[:], "CASIC V5.3.0.0")
+	copy(hw[:], "HW-1234-REV-A")
+	testMsgType[MonVer](t, MonVer{SwVersion: sw, HwVersion: hw})
+}
+
+func TestMonVerJSON(t *testing.T) {
+	var m MonVer
+	copy(m.SwVersion[:], "CASIC V5.3.0.0")
+	copy(m.HwVersion[:], "HW-1234-REV-A")
+	b, err := json.Marshal(&m)
+	if err != nil {
+		t.Fatalf("json.Marshal: %v", err)
+	}
+	s := string(b)
+	for _, want := range []string{
+		`"SwVersion":"CASIC V5.3.0.0"`,
+		`"HwVersion":"HW-1234-REV-A"`,
+	} {
+		if !strings.Contains(s, want) {
+			t.Errorf("JSON %s\nmissing %s", s, want)
+		}
+	}
+}
