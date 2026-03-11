e369c6dc72a03fe6b3286fa38880cd7d51ce21a8
James Clark <jjc@jclark.com>
2026-03-11 22:13:54 +0700

    Revert "Fix MODE checksum serialization to use XOR instead of CRC32"
    This reverts commit b91c54fb5daa3943c61dd9cd951d86eca26f3cf2.

--- gps/lib/uncmsg/mode.go
+++ gps/lib/uncmsg/mode.go
@@ -34,8 +34,6 @@ func (m *Mode) ID() (MsgID, string) {
 	return 0, "MODE"
 }
 
-func (m *Mode) usesXorChecksum() {}
-
 // Command returns the equivalent MODE command that would establish this mode
 func (m *Mode) Command() string {
 	cmd := m.Mode
