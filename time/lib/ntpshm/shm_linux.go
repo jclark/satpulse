4a1f896956fb9275796b7b7f3fe9197741ef6fb7
James Clark <jjc@jclark.com>
2026-06-26 06:45:11 +0100

    Fix ntpshm Linux go generate directives

--- time/lib/ntpshm/shm_linux.go
+++ time/lib/ntpshm/shm_linux.go
@@ -12,8 +12,6 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-//go:generate sh -c "{ echo '//go:build linux && (amd64 || arm64)'; echo; go tool cgo -godefs types_linux.go; } | gofmt > ztypes_linux.go && rm -rf _obj"
-
 var _ [expectedSize - unsafe.Sizeof(shmTime{})]byte
 var _ [unsafe.Sizeof(shmTime{}) - expectedSize]byte
 
--- time/lib/ntpshm/shm_sec_linux.go
+++ time/lib/ntpshm/shm_sec_linux.go
@@ -2,4 +2,6 @@
 
 package ntpshm
 
+//go:generate sh -c "{ echo '//go:build linux && (amd64 || arm64)'; echo; go tool cgo -godefs types_linux.go; } | gofmt > ztypes_linux.go && rm -rf _obj"
+
 type shmSec = int64
