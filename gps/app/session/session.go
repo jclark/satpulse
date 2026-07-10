67738dc9ea2709c57bb7c97aca3946c6d6f118c8
James Clark <jjc@jclark.com>
2026-07-10 19:31:30 +0700

    Revert "session: reject connections completed after disconnect"
    This reverts commit 394f3e3b0cbc175b433b8af855dca9c15d38b5d0.

--- gps/app/session/session.go
+++ gps/app/session/session.go
@@ -415,39 +415,24 @@ func (o SocketOpener) Socket() bool { return true }
 func (s *Session) Connect(op Opener, vendor gpsreg.Vendor) error {
 	s.mu.Lock()
 	s.closeLocked()
-	connCtx, connCancel := context.WithCancel(context.Background())
 	s.state = StateConnecting
-	s.connCtx = connCtx
-	s.connCancel = connCancel
 	s.mu.Unlock()
 	s.emit(EventState, StateConnecting)
-	ctx, cancel := context.WithTimeout(connCtx, connectOpenTimeout)
+	ctx, cancel := context.WithTimeout(context.Background(), connectOpenTimeout)
 	conn, speed, err := op.Open(ctx)
 	cancel()
 	if err != nil {
-		s.mu.Lock()
-		active := s.connCtx == connCtx && connCtx.Err() == nil
-		if active {
-			connCancel()
-			s.connCancel = nil
-			s.state = StateDisconnected
-		}
-		s.mu.Unlock()
-		if active {
-			s.emit(EventState, StateDisconnected)
-		}
+		s.setState(StateDisconnected)
 		return err
 	}
+	connCtx, connCancel := context.WithCancel(context.Background())
 	s.mu.Lock()
-	if s.connCtx != connCtx || connCtx.Err() != nil {
-		s.mu.Unlock()
-		conn.Close()
-		return context.Canceled
-	}
 	s.op = op
 	s.vendor = vendor
-	s.connWg.Go(func() { s.connManager(connCtx, conn, speed) })
+	s.connCtx = connCtx
+	s.connCancel = connCancel
 	s.mu.Unlock()
+	s.connWg.Go(func() { s.connManager(connCtx, conn, speed) })
 	return nil
 }
 
