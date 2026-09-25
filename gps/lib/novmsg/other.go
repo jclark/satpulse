722ebf196b51cb69fde6276c37721b32d4b17a94
James Clark <jjc@jclark.com>
2026-09-25 16:50:41 +0700

    nov: name the GPSEPHEM, QZSSEPHEMERIS, TRACKSTAT and HEADING2 IDs
    The table of known but unimplemented NovAtel-compatible binary message
    IDs did not include GPSEPHEM (7), QZSSEPHEMERIS (1336), TRACKSTAT (83)
    or HEADING2 (1335), although OEM7 and ByNav both document all four
    with these IDs, and SinoGNSS also documents HEADING2. Binary packets
    with these IDs were therefore shown by number: an M10 capture records
    GPSEPHEMB as "7" and TRACKSTATB as "83".
    
    Add them to the table so the packets are shown by name. They are named
    only, not decoded. SinoGNSS is not listed for GPSEPHEM, because its
    manual gives that log the ID 71.

--- gps/lib/novmsg/other.go
+++ gps/lib/novmsg/other.go
@@ -6,16 +6,20 @@ const (
 	// BestGNSSPosID defined in nav.go (OEM7, Bynav)
 	GalEphemerisID MsgID = 1122 // OEM6, Bynav, SinoGNSS, RTKLIB
 	GloEphemerisID MsgID = 723  // OEM7, Bynav, SinoGNSS, RTKLIB
+	GpsEphemID     MsgID = 7    // OEM7, Bynav
 	HeadingID      MsgID = 971  // OEM6, Bynav, SinoGNSS
+	Heading2ID     MsgID = 1335 // OEM7, Bynav, SinoGNSS
 	MarkTimeID     MsgID = 231  // OEM6, Bynav, SinoGNSS
 	// PsrDopID is defined in nav.go (has struct + registration)
 	// PsrPosID is defined in nav.go (OEM7, SinoGNSS)
 	// PsrVelID is defined in nav.go (OEM7, Bynav, SinoGNSS)
-	RangeCmpID     MsgID = 140  // OEM7, Bynav, SinoGNSS, Unicore, RTKLIB
-	RangeID        MsgID = 43   // OEM7, SinoGNSS, RTKLIB
-	RawEphemID     MsgID = 41   // OEM7, SinoGNSS, RTKLIB
-	RefStationID   MsgID = 175  // OEM7, SinoGNSS
-	VersionID      MsgID = 37   // OEM7, SinoGNSS
+	QzssEphemerisID MsgID = 1336 // OEM7, Bynav
+	RangeCmpID      MsgID = 140  // OEM7, Bynav, SinoGNSS, Unicore, RTKLIB
+	RangeID         MsgID = 43   // OEM7, SinoGNSS, RTKLIB
+	RawEphemID      MsgID = 41   // OEM7, SinoGNSS, RTKLIB
+	RefStationID    MsgID = 175  // OEM7, SinoGNSS
+	TrackStatID     MsgID = 83   // OEM7, Bynav
+	VersionID       MsgID = 37   // OEM7, SinoGNSS
 )
 
 func init() {
@@ -23,14 +27,18 @@ func init() {
 	// BestGNSSPosID registered in nav.go init()
 	idNameMap[GalEphemerisID] = "GALEPHEMERIS"
 	idNameMap[GloEphemerisID] = "GLOEPHEMERIS"
+	idNameMap[GpsEphemID] = "GPSEPHEM"
 	idNameMap[HeadingID] = "HEADING"
+	idNameMap[Heading2ID] = "HEADING2"
 	idNameMap[MarkTimeID] = "MARKTIME"
 	// PsrDopID registered in nav.go init()
 	// PsrPosID registered in nav.go init()
 	// PsrVelID registered in nav.go init()
+	idNameMap[QzssEphemerisID] = "QZSSEPHEMERIS"
 	idNameMap[RangeCmpID] = "RANGECMP"
 	idNameMap[RangeID] = "RANGE"
 	idNameMap[RawEphemID] = "RAWEPHEM"
 	idNameMap[RefStationID] = "REFSTATION"
+	idNameMap[TrackStatID] = "TRACKSTAT"
 	idNameMap[VersionID] = "VERSION"
 }
